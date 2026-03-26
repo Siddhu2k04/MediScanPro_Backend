535fb0874a977c18abe978a2cf95a35887fd2b0e
shravanirwani-cell <shravanirwani@gmail.com>
2026-03-26 16:23:37 +0530

    fixed indentation properly final

--- app.py
+++ app.py
@@ -75,11 +75,6 @@ def predict():
 # RUN APP
 # =========================
 
-# =========================
-# RUN APP
-# =========================
-
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
